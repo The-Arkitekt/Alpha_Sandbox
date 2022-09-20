63770d48cf746bd99526fc6e5fede5c99de07ed7
David Crumley <daviejones@DESKTOP-65U9I6F.localdomain>
2022-09-20 15:18:08 -0400

    Added printout of message

--- src/common/HardwareInterface/SerialInterface.cpp
+++ src/common/HardwareInterface/SerialInterface.cpp
@@ -66,6 +66,7 @@ int SerialInterface::initPort(int vTime, int vMin) {
 	tty.c_lflag &= ~ECHOE;															// Disable erasure
 	tty.c_lflag &= ~ECHONL;															// Disable new-line echo
 	tty.c_lflag &= ~ISIG;															// Disable interpretation of INTR, QUIT and SUSP
+	tty.c_lflag &= ~IEXTEN;
 	tty.c_iflag &= ~(IXON | IXOFF | IXANY);											// Turn off s/w flow ctrl
 
 	tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);	// Disable any special handling of received bytes
