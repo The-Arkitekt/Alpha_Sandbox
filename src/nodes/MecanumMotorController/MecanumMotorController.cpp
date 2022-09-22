5dabb279b7315cbee16702ea12788b3ba44a64ab
David Crumley <daviejones@DESKTOP-65U9I6F.localdomain>
2022-09-22 10:55:16 -0400

    yeah yeah

--- src/nodes/MecanumMotorController/MecanumMotorController.cpp
+++ src/nodes/MecanumMotorController/MecanumMotorController.cpp
@@ -83,10 +83,11 @@ bool MecanumMotorController::applyMotorSpeeds() {
 	//initialize serial port to have a max 100 ms blocking and no byte minimum
 	if (serial.initPort(10, 0) < 0)
 		return false;
+	/*
 	std::vector<int8_t> test{ 1 };
 	if (!serial.writeData(test))
 		return false;
-	/*
+	*/
 	std::vector<int8_t> readBuf;
 	if (!serial.readData(&readBuf, 1))
 		return false;
@@ -97,7 +98,7 @@ bool MecanumMotorController::applyMotorSpeeds() {
 		std::cout << int(readBuf[i]);
 	}
 	std::cout << std::endl;	
-	*/
+	
 	serial.closePort();
 	return true;
 }
